5a1c91366d43f911680231fead2b680b9dd56be1
BAKelly12 <kellyk12@seattleu.edu>
2019-01-16 20:29:48 -0800

    Added command prompt and tokenizing string

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -5,3 +5,27 @@
  */
 
 
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+        //Print the shell prompt, separate the string into vectors separated by space
+        cout << "MyShell$ ";
+        string input;
+        getline(cin,input);
+        cout << input << endl;
+        stringstream check1(input);
+        vector <string> tokens;
+        string intermediate;
+        while(getline(check1, intermediate, ' ')){
+                tokens.push_back(intermediate);
+        } 
+        for(unsigned int i = 0; i < tokens.size(); i++)
+                cout << tokens[i] << endl;
+}
+
+
+

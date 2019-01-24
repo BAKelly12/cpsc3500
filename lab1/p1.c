6040504ac01d3cc43822a1169671973d23974b9d
BAKelly12 <kellyk12@seattleu.edu>
2019-01-23 18:41:50 -0800

    Fully working except child exit status output. Added moby.txt for pulling and testing.  Updated README.  Updated Makefile.  Updated Comments

--- lab1/p1.c
+++ lab1/p1.c
@@ -1,11 +1,11 @@
-/**
- * @file labx.c
- * @description implemntation of a rudimentary command-
- *          line shell
- * @Authors Sam Van Nes, Brigid Kelly
- * @see "Seattle University CPSC3500 Winter 2019"
+ /**
+ *@file p1.c
+ *@description implemntation of a rudimentary command
+ *               line shell
+ *@Authors Sam Van Nes, Brigid Kelly
+ *@see "Seattle University CPSC3500 Winter 2019
  */
-
+  
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
@@ -16,64 +16,27 @@ struct command{
 	const char **argv;
 };
 
-int parseIntoCommands();
-int main()
-{
-	//New program
-	parseIntoCommands();
-    return 1;
+//Function declarations
+void parseIntoCommandTable();
+pid_t spawn_proc(int in, int out, struct command *cmd);
+void fork_pipes (int n, struct command *cmd);
+
+
+int main(){	
+	    parseIntoCommandTable();
+        return 1;
 }
 
 /**
- * @brief
- * @param
- * @return
+ * @brief Called by main to take in command from command-line, parses 
+ *          line into tokens, creates command array structure and calls
+ *          fork_pipes function
+ * @param N/A
+ * @return N/A 
  */
 
-int fork_pipes (int n, struct command *cmd){
-	int i;
-	pid_t pid;
-	int in, fd[2];
-	in = 0;
-	for (i = 0; i < n-1; i++) {
-		pipe (fd);
-		spawn_proc (in, fd[1], cmd + i);
-		close (fd[1]);
-		in = fd [0];
-	}
-	if (in != 0)
-		dup2 (in, 0);
-	return execvp( cmd[i].argv[0], (char* const*) cmd[i].argv);
-}
-
-/**
- * @brief
- * @param
- * @return
- */
-
-int spawn_proc (int in, int out, struct command *cmd){
-	pid_t pid;
-	if ((pid = fork()) == 0) {
-		if (in != 0) {
-			dup2 (in, 0);
-			close (in);
-		} if (out != 1) {
-			dup2 (out, 1);
-			close (out);
-		}
-		return execvp( cmd->argv[0], (char* const *)cmd->argv);
-	}
-	return pid;	
-}
-
-/**
- * @brief receives input from stdin and parses the line for tokens and delimiters
- *        delims include{' , " , \t , \n, \r, | , " " }
- * @param none
- * @return int - success or failure of fork_pipes function
- */
-int parseIntoCommands(){
+void parseIntoCommandTable(){
+	
 	int max = 100; //User string length
 	char getPlz[max]; //Actual string given by user
   
@@ -103,7 +66,7 @@ int parseIntoCommands(){
 			//Reset for next command
 			colBuf = 0;
 			rowBuf++;
-			stringIndex++; //This means its on a special char, so go past it.
+			stringIndex++; //itr on Special char, so go past it.
 	
 		} else if (getPlz[stringIndex] == '\"') { //Double quote parce	
   		stringIndex++;
@@ -117,7 +80,7 @@ int parseIntoCommands(){
 			//Reset for next command
 			colBuf = 0;
 			rowBuf++;
-			stringIndex++; //This means its on a special char, so go past it.
+			stringIndex++; //itr on Special char, so go past it.
 		
 		} else if (getPlz[stringIndex] == '|') {
 			numPipes++;
@@ -127,8 +90,8 @@ int parseIntoCommands(){
 			colBuf = 0;
 			stringIndex++;
 
-		} else if (getPlz[stringIndex] != ' ') { //Some character parce	
-			while (getPlz[stringIndex] != ' ' && stringIndex < strLength) {
+		} else if (getPlz[stringIndex] != ' ') { //Some character parse	
+			while (getPlz[stringIndex] != ' ' && stringIndex < strLength && getPlz[stringIndex] != '|'){
 				buf[rowBuf][colBuf] = getPlz[stringIndex];
 				colBuf++;
 				stringIndex++;
@@ -142,8 +105,7 @@ int parseIntoCommands(){
 		} else
 			stringIndex++;
 	}
-    
-    //Init vars and copy buffer into args array
+ 
 	char* args[50];
 	int i;	
 	for (i = 0; i < rowBuf; i++)
@@ -164,9 +126,70 @@ int parseIntoCommands(){
 			counter++;
 		}
 	}
-    //terminating command array with NULL
 	command[commandNumber][counter] = NULL;
-	struct command cmd[] = {command[0], command[1]};	
-	return fork_pipes (numPipes + 1, cmd);
+	
+	//Assuming max of 10 commands, make structure array that contains 
+	//all possible commands. If less that the max, the forkPipes tells
+	//how many 
+	struct command cmd[] = {command[0], command[1], command[2], command[3],
+							command[4], command[5], command[6], command[7],
+							command[8], command[9]};
+    
+	 fork_pipes(numPipes + 1, cmd);
+}
+
+/**
+ * @brief Creates pips for all processes being executed
+ * @param n: Number of pipes
+ *        cmd:  Pointer to command structure containing all 
+ *              character arrays for input to execvp
+ * @return N/A
+ */
+
+void fork_pipes (int n, struct command *cmd){
+	int i;
+	pid_t pid;
+	int in, fd[2];
+	in = 0;
+	for (i = 0; i < n-1; i++) {
+		pipe (fd);
+		pid = spawn_proc (in, fd[1], cmd + i);
+		close (fd[1]);
+		in = fd [0];
+	}
+	if (in != 0)
+		dup2 (in, 0);
+
+     unsigned es = execvp(cmd[i].argv[0], (char* const*) cmd[i].argv);
+     if(!es)
+             perror("Failed to execute.");
+} 
+
+
+/**
+ * @brief Creates child processes, links pipes, closes parent input file 
+ *          descriptors and executes next command in child processes
+ * @param in:   Input file descriptor
+ *        out:  Output file descriptor
+ * @return pid: process ID of child process (does not turn if
+ *              process is child process).
+ */
+
+pid_t spawn_proc (int in, int out, struct command *cmd){
+	pid_t pid;
+    pid = fork(); 
+	if (!pid) {//This is the child process
+		if (in != 0) {
+			dup2 (in, 0);
+			close (in);
+		} if (out != 1) {
+			dup2 (out, 1);
+			close (out);
+		}
+        execvp( cmd->argv[0], (char* const *)cmd->argv);
+    }else if(pid<0)
+            perror("Something went wrong 8).. ");
+
+   return pid;	
 }
 
